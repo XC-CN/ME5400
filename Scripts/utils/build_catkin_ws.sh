753f47ff9f07de654861cf87d51166369b6fce8f
XC-CN <xc@example.com>
2026-04-08 20:30:41 +0800

    解决编译问题

--- Scripts/utils/build_catkin_ws.sh
+++ Scripts/utils/build_catkin_ws.sh
@@ -7,4 +7,8 @@ CATKIN_WS="$ROOT_DIR/catkin_ws"
 
 source /opt/ros/noetic/setup.bash
 cd "$CATKIN_WS"
-catkin_make
+
+# Use env -i to completely isolate the build from the current (Conda) environment.
+# We only pass through HOME and a minimal PATH, then source ROS and build.
+env -i HOME="$HOME" USER="$USER" PATH="/usr/bin:/bin:/opt/ros/noetic/bin" \
+    bash -c "source /opt/ros/noetic/setup.bash && cd \"$CATKIN_WS\" && catkin_make -DPYTHON_EXECUTABLE=/usr/bin/python3 -DCATKIN_ENABLE_TESTING=OFF"
